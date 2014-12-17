2dcb9be08eb43d3ea31f39dd752c314aeb58f60d
Daryl Laurange <dlaurange@vsi-corp.com>
2014-12-17 12:40:43 -0500

    #CA-45 Fixed bug where python whitenoise would crash when using an odd number for xfer_len

--- python/Waveform.py
+++ python/Waveform.py
@@ -55,6 +55,10 @@ class Waveform:
     # @param spa  Scalars per atom, 2 for Complex
     # @return the new data buffer
     def whitenoise(self, sdev, n, spa=1):
+        #CA-45 When odd number of elements, reduce by one to avoid referencing array element
+        # that is out of bounds in outbuff[i+1]
+        if (n % 2) == 1:
+            n -= 1
         outbuff = range(n*spa)
         v1 = 0.0; v2 = 0.0; sum1 = 0.0
         fdev = float(sdev)
