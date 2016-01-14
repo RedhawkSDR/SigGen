a9dfbc04f61f9e2b1c5b5684c780feb9b62891ce
Kipp Bowen <jkb@axiosengineering.com>
2016-01-14 16:07:54 -0500

    Increment sub-minor version post-release (CA-318, CA-343).

--- build.sh
+++ build.sh
@@ -22,9 +22,9 @@ if [ "$1" = "rpm" ]; then
     if [ -e rh.SigGen.spec ]; then
         mydir=`dirname $0`
         tmpdir=`mktemp -d`
-        cp -r ${mydir} ${tmpdir}/rh.SigGen-2.0.0
-        tar czf ${tmpdir}/rh.SigGen-2.0.0.tar.gz --exclude=".svn" -C ${tmpdir} rh.SigGen-2.0.0
-        rpmbuild -ta ${tmpdir}/rh.SigGen-2.0.0.tar.gz
+        cp -r ${mydir} ${tmpdir}/rh.SigGen-2.0.1
+        tar czf ${tmpdir}/rh.SigGen-2.0.1.tar.gz --exclude=".svn" -C ${tmpdir} rh.SigGen-2.0.1
+        rpmbuild -ta ${tmpdir}/rh.SigGen-2.0.1.tar.gz
         rm -rf $tmpdir
     else
         echo "Missing RPM spec file in" `pwd`
--- rh.SigGen.spec
+++ rh.SigGen.spec
@@ -28,7 +28,7 @@ Prefix:         %{_prefix}
 %define _infodir       %{_prefix}/info
 
 Name:           rh.SigGen
-Version:        2.0.0
+Version:        2.0.1
 Release:        1%{?dist}
 Summary:        Component %{name}
 
